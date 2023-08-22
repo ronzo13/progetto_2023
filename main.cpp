d85717bb89021828d2cfc589561f6c770dda2376
Sara Maiano <sara.maiano@studio.unibo.it>
2023-08-22 15:32:00 +0200

    controlli nel main + test

--- main.cpp
+++ main.cpp
@@ -11,6 +11,10 @@ int main() {
     std::cout << "beta: ";
     std::string input;
     std::cin >> input;
+    if (input.find(',') != std::string::npos) {
+      std::cout << "Invalid value for beta. Please use a dot (.) as the decimal separator." << '\n';
+      continue; // Ask for input again
+    }
     try {
       double check_beta = std::stod(input);  // qui avviene il troncamento
       // riparte anche se inserisco int
@@ -28,17 +32,53 @@ int main() {
     }
   }
 
-  std::cout << "gamma: ";
-  std::cin >> param.gamma;
+  valid_input = false;
+
+  while (!valid_input) {
+    std::cout << "gamma: ";
+    std::string input;
+    std::cin >> input;
+    if (input.find(',') != std::string::npos) {
+      std::cout << "Invalid value for beta. Please use a dot (.) as the decimal separator." << '\n';
+      continue; // Ask for input again
+    }
+    try {
+      double check_gamma = std::stod(input);  // qui avviene il troncamento
+      // riparte anche se inserisco int
+      if (check_gamma >= 0 && check_gamma <= 1) {
+        valid_input = true;
+      } else {
+        std::cout
+            << "Invalid value for gamma. Please enter a number between 0 and 1."
+            << '\n';
+      }
+    } catch (const std::invalid_argument& e) {
+      std::cout
+          << "Invalid value for gamma. Please enter a number between 0 and 1."
+          << '\n';
+    }
+  }
+
+
+
+  //std::cout << "gamma: ";
+  //std::cin >> param.gamma;
 
   int new_s{};
   int new_i{};
   int new_r{};
 
+  valid_input = false;
+
+
   while (!valid_input) {
     std::cout << "s: ";
     std::string input;
     std::cin >> input;
+    if (input.find(',') != std::string::npos) {
+      std::cout << "Invalid value for beta. Please use a dot (.) as the decimal separator." << '\n';
+      continue; // Ask for input again
+    }
     try {
       double check_s = std::stod(input);  // qui avviene il troncamento
       // riparte anche se inserisco int
@@ -56,17 +96,100 @@ int main() {
 
   // std::cout << "s: ";
   // std::cin >> new_s;
-  std::cout << "i: ";
-  std::cin >> new_i;
-  std::cout << "r: ";
-  std::cin >> new_r;
+
+  valid_input = false;
+
+
+
+while (!valid_input) {
+    std::cout << "i: ";
+    std::string input;
+    std::cin >> input;
+    if (input.find(',') != std::string::npos) {
+      std::cout << "Invalid value for beta. Please use a dot (.) as the decimal separator." << '\n';
+      continue; // Ask for input again
+    }
+    try {
+      double check_i = std::stod(input);  // qui avviene il troncamento
+      // riparte anche se inserisco int
+      if (check_i == static_cast<int>(check_i) && check_i >= 0) {
+        new_i = static_cast<int>(check_i);
+        valid_input = true;
+      } else {
+        std::cout << "Invalid value for i. Please enter a valid integer."
+                  << '\n';
+      }
+    } catch (const std::invalid_argument& e) {
+      std::cout << "Invalid value for i. Please enter a valid integer." << '\n';
+    }
+  }
+
+  // std::cout << "i: ";
+  // std::cin >> new_i;
+
+  valid_input = false;
+
+
+
+while (!valid_input) {
+    std::cout << "r: ";
+    std::string input;
+    std::cin >> input;
+    if (input.find(',') != std::string::npos) {
+      std::cout << "Invalid value for beta. Please use a dot (.) as the decimal separator." << '\n';
+      continue; // Ask for input again
+    }
+    try {
+      double check_r = std::stod(input);  // qui avviene il troncamento
+      // riparte anche se inserisco int
+      if (check_r == static_cast<int>(check_r) && check_r >= 0) {
+        new_r = static_cast<int>(check_r);
+        valid_input = true;
+      } else {
+        std::cout << "Invalid value for r. Please enter a valid integer."
+                  << '\n';
+      }
+    } catch (const std::invalid_argument& e) {
+      std::cout << "Invalid value for r. Please enter a valid integer." << '\n';
+    }
+  }
+
+  // std::cout << "r: ";
+  // std::cin >> new_r;
 
   State state(new_s, new_i, new_r);
   SIR sir{state, param};
 
   int days{};
-  std::cout << "epidemic duration: ";
-  std::cin >> days;
+
+  valid_input = false;
+
+  while (!valid_input) {
+    std::cout << "epidemic duration : ";
+    std::string input;
+    std::cin >> input;
+    if (input.find(',') != std::string::npos) {
+      std::cout << "Invalid value for beta. Please use a dot (.) as the decimal separator." << '\n';
+      continue; // Ask for input again
+    }
+    try {
+      double check_days = std::stod(input);  // qui avviene il troncamento
+      // riparte anche se inserisco int
+      if (check_days == static_cast<int>(check_days) && check_days >= 0) {
+        days = static_cast<int>(check_days);
+        valid_input = true;
+      } else {
+        std::cout << "Invalid value for epidemic duration. Please enter a valid integer."
+                  << '\n';
+      }
+    } catch (const std::invalid_argument& e) {
+      std::cout << "Invalid value for epidemic duration. Please enter a valid integer." << '\n';
+    }
+  }
+
+
+  // std::cout << "epidemic duration: ";
+  // std::cin >> days;
 
   std::cout << "total: " << sir.get_total() << '\n';
   std::cout << "------" << '\n';
