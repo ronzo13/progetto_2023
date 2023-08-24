4fa899fa66766383096dc3d4ebb7ffa081445a54
ronzo13 <federico.tronchi@studio.unibo.it>
2023-08-24 14:44:22 +0200

    file separato per le funzioni di input

---
+++ validate_input.hpp
@@ -0,0 +1,24 @@
+#include "sir.hpp"
+
+#ifndef VALIDATE_INPUT_HPP
+#define VALIDATE_INPUT_HPP
+
+double valid_beta(double);
+
+double valid_gamma(double);
+
+void valid_R0(Param&);
+
+int valid_s(int);
+
+int valid_i(int);
+
+int valid_r(int);
+
+int valid_days(int);
+
+double valid_S_percentage(double);
+
+double valid_I_percentage(double);
+
+#endif
